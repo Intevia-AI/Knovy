fcb1990bb0e5b579599babe6af77c077207b6a91
Marx <b10705052@ntu.edu.tw>
2025-04-10 16:17:50 +0800

    code

---
+++ apps/web/app/(landing)/layout.tsx
@@ -0,0 +1,16 @@
+import FooterSection from "@/components/footer";
+import { HeroHeader } from "@/components/hero-header";
+
+export default function LandingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <HeroHeader />
+      {children}
+      <FooterSection />
+    </>
+  );
+}
---
+++ apps/web/app/(landing)/page.tsx
@@ -0,0 +1,17 @@
+import CallToAction from "@/components/call-to-action";
+import { FeaturesSection } from "@/components/features";
+import { HeroSection } from "@/components/hero-section";
+import { IntegrationsSection } from "@/components/integrations";
+import { TeamSection } from "@/components/team";
+
+export default function Page() {
+  return (
+    <>
+      <HeroSection />
+      <FeaturesSection />
+      <IntegrationsSection />
+      <TeamSection />
+      <CallToAction />
+    </>
+  );
+}
