0f90bbc5209a4f4c43681383659deb4c87f9ff7e
Tweizh <ghnmqdtg@gmail.com>
2025-08-16 00:15:55 +0800

    Fix(web): Revert the wrong update on tailwind settings in `package/ui`

--- packages/ui/postcss.config.mjs
+++ packages/ui/postcss.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('postcss-load-config').Config} */
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-import tailwindConfig from './tailwind.config.ts';
-
 const config = {
-  plugins: [
-    tailwindcss(tailwindConfig),
-    autoprefixer,
-  ],
+    plugins: { "@tailwindcss/postcss": {} },
 };
 
 export default config;
