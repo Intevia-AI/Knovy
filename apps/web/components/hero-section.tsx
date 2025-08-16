d670be2a2823abc1a00eff7f30718a241c5a6991
Tweizh <ghnmqdtg@gmail.com>
2025-08-16 15:38:48 +0800

    Misc(web): Update titles in hero section

--- apps/web/components/hero-section.tsx
+++ apps/web/components/hero-section.tsx
@@ -3,8 +3,7 @@
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
 import { TextEffect } from "@workspace/ui/components/text-effect";
-import { DemoComponent } from "@/components/demo";
-import { Button } from "@workspace/ui/components/button";
+// import { Button } from "@workspace/ui/components/button";
 import { useState } from "react";
 
 export function HeroSection() {
@@ -57,19 +56,18 @@ export function HeroSection() {
         >
           <div className="w-140 h-320 -translate-y-87.5 absolute left-0 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]" />
         </div>
-        <section>
-          <div className="relative pt-24 md:pt-36">
+        <section className="h-screen flex items-center justify-center">
+          <div className="relative -mt-12">
             <div className="absolute inset-0 -z-10 size-full [background:radial-gradient(125%_125%_at_50%_100%,transparent_0%,var(--color-background)_75%)]"></div>
             <div className="mx-auto max-w-7xl px-6">
               <div className="text-center sm:mx-auto lg:mr-auto lg:mt-0">
                 <Link
-                  href="#link"
+                  href="#"
                   className="hover:bg-background dark:hover:border-t-border bg-muted group mx-auto flex w-fit items-center gap-4 rounded-full border p-1 pl-4 shadow-md shadow-zinc-950/5 transition-colors duration-300 dark:border-t-white/5 dark:shadow-zinc-950"
                 >
                   <span className="text-foreground text-sm">
-                    隆重介紹：全方位AI開會神器
+                    隆重介紹：全方位 AI 開會神器
                   </span>
-                  <span className="dark:border-background block h-4 w-0.5 border-l bg-white dark:bg-zinc-700"></span>
                   <div className="bg-background group-hover:bg-muted size-6 overflow-hidden rounded-full duration-500">
                     <div className="flex w-12 -translate-x-1/2 duration-500 ease-in-out group-hover:translate-x-0">
                       <span className="flex size-6">
@@ -86,35 +84,21 @@ export function HeroSection() {
                   preset="fade-in-blur"
                   speedSegment={0.3}
                   as="h1"
-                  className="mt-8 text-balance text-6xl md:text-7xl lg:mt-16 xl:text-[5.25rem]"
+                  className="mt-8 text-balance text-6xl md:text-7xl lg:mt-16 xl:text-[7.25rem]"
                 >
-                  您的個人會議神器 – INTEVIA AI
+                  INTEVIA AI
                 </TextEffect>
                 <TextEffect
                   per="line"
                   preset="fade-in-blur"
                   speedSegment={0.3}
                   delay={0.5}
-                  as="p"
-                  className="mx-auto mt-8 max-w-2xl text-balance text-lg"
+                  as="h3"
+                  className="mt-4 text-balance text-xl md:text-2xl lg:mt-8 xl:text-[2.20rem]"
                 >
-                  全方位AI開會神器
-                </TextEffect>
-                <TextEffect
-                  per="line"
-                  preset="fade-in-blur"
-                  speedSegment={0.3}
-                  delay={0.5}
-                  as="p"
-                  className="mx-auto mt-8 max-w-2xl text-balance text-lg"
-                >
-                  Your Personal Meeting Cheater – INTEVIA AI
+                  Your All-in-One AI working assistant
                 </TextEffect>
               </div>
-            </div>
-
-            <div className="mx-auto max-w-5xl p-6 mt-12 lg:mt-16">
-              <DemoComponent />
             </div>
 
             {/* Feedback Section */}
