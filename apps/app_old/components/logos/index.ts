7b3d44708cdaba07128d80d1b39e97238bb8ef13
Tweizh <ghnmqdtg@gmail.com>
2025-10-13 14:16:00 +0800

    Feat(app): Remove the old app

--- apps/app_old/components/logos/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Gemini } from "./Gemini.jsx";
-export { default as Replit } from "./Replit.jsx";
-export { default as MagicUI } from "./MagicUI.jsx";
-export { default as VSCodium } from "./VSCodium.jsx";
-export { default as MediaWiki } from "./MediaWiki.jsx";
-export { default as GooglePaLM } from "./GooglePaLM.jsx";
